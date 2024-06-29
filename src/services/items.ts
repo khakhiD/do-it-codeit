d7988125a49a9a223c25b3f84b3e1239646243e6
khakhiD <kakkiid@gmail.com>
2024-06-29 19:25:00 +0900

    fix: Items API 응답 타입 지정

--- src/services/items.ts
+++ src/services/items.ts
@@ -1,30 +1,20 @@
-import { ItemPatchRequest, ItemPostRequest } from '@/types/types';
+import {
+  ItemDeleteResponse,
+  ItemDetailGetResponse,
+  ItemGetResponse,
+  ItemPatchRequest,
+  ItemPostRequest,
+  ItemPostResponse,
+} from '@/types/types';
 
 // API URL 및 TENANT ID
 const TENANT_ID = 'khakhid';
 const API_BASE_URL = new URL('https://assignment-todolist-api.vercel.app/api');
 
-// 응답 에러 체크
-const checkResponse = async (response: Response) => {
-  if (!response.ok) {
-    const errorData = await response.json();
-    throw new Error(errorData.message || '응답 에러 발생!');
-  }
-  return response.json();
-};
-
 // APIs
-export const getItems = async () => {
-  const response = await fetch(`${API_BASE_URL}/${TENANT_ID}/items`);
-  return checkResponse(response);
-};
-
-export const getItemDetail = async (itemId: number) => {
-  const response = await fetch(`${API_BASE_URL}/${TENANT_ID}/items/${itemId}`);
-  return checkResponse(response);
-};
-
-export const postItem = async (data: ItemPostRequest) => {
+export const postItem = async (
+  data: ItemPostRequest
+): Promise<ItemPostResponse> => {
   const response = await fetch(`${API_BASE_URL}/${TENANT_ID}/items`, {
     method: 'POST',
     headers: {
@@ -36,7 +26,22 @@ export const postItem = async (data: ItemPostRequest) => {
   return checkResponse(response);
 };
 
-export const patchItem = async (data: ItemPatchRequest, itemId: number) => {
+export const getItems = async (): Promise<ItemGetResponse[]> => {
+  const response = await fetch(`${API_BASE_URL}/${TENANT_ID}/items`);
+  return checkResponse(response);
+};
+
+export const getItemDetail = async (
+  itemId: number
+): Promise<ItemDetailGetResponse> => {
+  const response = await fetch(`${API_BASE_URL}/${TENANT_ID}/items/${itemId}`);
+  return checkResponse(response);
+};
+
+export const patchItem = async (
+  data: ItemPatchRequest,
+  itemId: number
+): Promise<ItemDeleteResponse> => {
   const response = await fetch(`${API_BASE_URL}/${TENANT_ID}/items/${itemId}`, {
     method: 'PATCH',
     headers: {
@@ -54,4 +59,12 @@ export const deleteItem = async (itemId: number) => {
   });
 
   return checkResponse(response);
+};
+
+const checkResponse = async (response: Response) => {
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || '응답 에러 발생!');
+  }
+  return response.json();
 };
