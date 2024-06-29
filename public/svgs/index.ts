179cce7451118236c923d44167b56609b1a4cb78
khakhiD <kakkiid@gmail.com>
2024-06-29 21:32:07 +0900

    feat: svg 컴포넌트로 사용

---
+++ public/svgs/index.ts
@@ -0,0 +1,23 @@
+import EditIcon from './edit.svg';
+import PlusIcon from './plus.svg';
+import PlusGrayIcon from './plus_gray.svg';
+import CheckIcon from './check.svg';
+import CheckedIcon from './checked.svg';
+import UncheckedIcon from './unchecked.svg';
+import CancelIcon from './cancel.svg';
+import LogoLarge from './logo_large.svg';
+import LogoSmall from './logo_small.svg';
+import ImageIcon from './img.svg';
+
+export {
+  EditIcon,
+  PlusGrayIcon,
+  PlusIcon,
+  CancelIcon,
+  CheckIcon,
+  CheckedIcon,
+  UncheckedIcon,
+  LogoLarge,
+  LogoSmall,
+  ImageIcon,
+};
